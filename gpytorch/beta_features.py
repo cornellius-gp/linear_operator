3fe6d28c2c8f8e790cf89dc6c4c07f48368b5f2b
Geoff Pleiss <gpleiss@gmail.com>
2018-01-30 15:28:37 -0500

    Contexts -> beta features

---
+++ gpytorch/beta_features.py
@@ -0,0 +1,32 @@
+class _feature_flag(object):
+    _state = False
+
+    @classmethod
+    def on(cls):
+        return cls._state
+
+    @classmethod
+    def _set_state(cls, state):
+        cls._state = state
+
+    def __init__(self):
+        self.prev = self.__class__.on()
+
+    def __enter__(self):
+        self.__class__._set_state(True)
+
+    def __exit__(self, *args):
+        self.__class__._set_state(False)
+        return False
+
+
+class fast_pred_var(_feature_flag):
+    """
+    Fast predictive variances - with Lanczos
+    """
+    pass
+
+
+__all__ = [
+    fast_pred_var,
+]
