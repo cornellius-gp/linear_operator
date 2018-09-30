0f9780f85c42961b1f294efb525e25ad5a8aec22
Geoff Pleiss <gpleiss@gmail.com>
2018-09-30 11:10:03 -0400

    Reorganize utils
    - Move all functions out of __init__ and into appropriate submodule
    - Move `approx_equals` into test/_utils.py (since we only use it for
      tests)
    - Rename test/util to test/utils
    
    BREAKING CHANGE: (for users)
    - `gpytorch.utils.scale_to_bounds` is now
      `gpytorch.utils.grid.scale_to_bounds`
    
    Important for developers:
    - `gpytorch.utils.approx_equals` is moved into a test-specific folder

---
+++ test/utils/test_cholesky.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import torch
+import unittest
+from test._utils import approx_equal
+from gpytorch.utils.cholesky import tridiag_batch_potrf, tridiag_batch_potrs
+
+
+class TestTriDiag(unittest.TestCase):
+    def test_potrf(self):
+        chol = torch.tensor([[1, 0, 0, 0], [2, 1, 0, 0], [0, 1, 2, 0], [0, 0, 2, 3]], dtype=torch.float).unsqueeze(0)
+        trid = chol.matmul(chol.transpose(-1, -2))
+
+        self.assertTrue(torch.equal(chol, tridiag_batch_potrf(trid, upper=False)))
+
+    def test_potrs(self):
+        chol = torch.tensor([[1, 0, 0, 0], [2, 1, 0, 0], [0, 1, 2, 0], [0, 0, 2, 3]], dtype=torch.float).unsqueeze(0)
+
+        mat = torch.randn(1, 4, 3)
+        self.assertTrue(
+            approx_equal(torch.potrs(mat[0], chol[0], upper=False), tridiag_batch_potrs(mat, chol, upper=False)[0])
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
