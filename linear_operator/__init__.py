6e541b65d87f352724e7e260d31bb5ccb19daef6
Geoff Pleiss <gpleiss@gmail.com>
2022-05-24 13:11:02 +0000

    Add LinearOperator to package base

--- linear_operator/__init__.py
+++ linear_operator/__init__.py
@@ -11,28 +11,29 @@ from .functions import (  # Deprecated
     root_decomposition,
     root_inv_decomposition,
 )
-from .operators import cat, to_dense, to_linear_operator
+from .operators import LinearOperator, cat, to_dense, to_linear_operator
 
 __version__ = "0.0.1"
 
 __all__ = [
+    # Base class
+    "LinearOperator",
     # Submodules
     "operators",
     "utils",
     # Functions
     "cat",
-    "to_dense",
     "dsmm",
     "inv_matmul",
     "inv_quad",
     "inv_quad_logdet",
-    "to_linear_operator",
     "logdet",
-    "log_normal_cdf",
     "matmul",
     "pivoted_cholesky",
     "root_decomposition",
     "root_inv_decomposition",
+    "to_dense",
+    "to_linear_operator",
     # Context managers
     "beta_features",
     "settings",
