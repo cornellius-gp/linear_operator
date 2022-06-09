ea4f9c85e92ad3f76c87d38f272386f2dc202144
Geoff Pleiss <gpleiss@gmail.com>
2022-06-09 17:08:56 +0000

    Remove unused utilities

--- linear_operator/utils/__init__.py
+++ linear_operator/utils/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from . import broadcasting, cholesky, grid, interpolation, lanczos, permutation, sparse, warnings
+from . import broadcasting, cholesky, errors, getitem, interpolation, lanczos, permutation, sparse, warnings
 from .contour_integral_quad import contour_integral_quad
 from .linear_cg import linear_cg
 from .memoize import cached
@@ -9,34 +9,22 @@ from .pinverse import stable_pinverse
 from .qr import stable_qr
 from .stochastic_lq import StochasticLQ
 
-
-def prod(items):
-    """ """
-    if len(items):
-        res = items[0]
-        for item in items[1:]:
-            res = res * item
-        return res
-    else:
-        return 1
-
-
 __all__ = [
     "broadcasting",
     "cached",
-    "contour_integral_quad",
-    "linear_cg",
-    "StochasticLQ",
     "cholesky",
-    "grid",
+    "contour_integral_quad",
+    "errors",
+    "getitem",
     "interpolation",
     "lanczos",
+    "linear_cg",
     "minres",
     "permutation",
     "pinverse",
-    "prod",
     "sparse",
     "stable_pinverse",
     "stable_qr",
     "warnings",
+    "StochasticLQ",
 ]
