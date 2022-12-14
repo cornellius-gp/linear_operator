373c421249c019a184f497ef4228fd09a7f9df44
Corwin Joy <corwinjoy@gmail.com>
2022-12-14 14:42:31 -0800

    Upgraded signatures and tests for matmul_linear_operator.py

--- linear_operator/operators/matmul_linear_operator.py
+++ linear_operator/operators/matmul_linear_operator.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
+from typing import Optional, Tuple, Union
+
 import torch
+from jaxtyping import Float
+from torch import Tensor
 
 from ..utils.broadcasting import _matmul_broadcast_shape, _pad_with_singletons
 from ..utils.getitem import _noop_index
 from ..utils.memoize import cached
-from ._linear_operator import LinearOperator
+from ._linear_operator import IndexType, LinearOperator
 from .dense_linear_operator import DenseLinearOperator, to_linear_operator
 from .diag_linear_operator import DiagLinearOperator
 
@@ -41,12 +45,14 @@ class MatmulLinearOperator(LinearOperator):
         else:
             self.right_linear_op = right_linear_op
 
-    def _expand_batch(self, batch_shape):
+    def _expand_batch(
+        self: Float[LinearOperator, "... M N"], batch_shape: torch.Size
+    ) -> Float[LinearOperator, "... M N"]:
         return self.__class__(
             self.left_linear_op._expand_batch(batch_shape), self.right_linear_op._expand_batch(batch_shape)
         )
 
-    def _get_indices(self, row_index, col_index, *batch_indices):
+    def _get_indices(self, row_index: IndexType, col_index: IndexType, *batch_indices: IndexType) -> torch.Tensor:
         row_index = row_index.unsqueeze(-1)
         col_index = col_index.unsqueeze(-1)
         batch_indices = tuple(batch_index.unsqueeze(-1) for batch_index in batch_indices)
@@ -62,7 +68,7 @@ class MatmulLinearOperator(LinearOperator):
         res = (left_tensor * right_tensor).sum(-1)
         return res
 
-    def _diagonal(self):
+    def _diagonal(self: Float[LinearOperator, "*batch N N"]) -> Float[torch.Tensor, "... N"]:
         if isinstance(self.left_linear_op, DenseLinearOperator) and isinstance(
             self.right_linear_op, DenseLinearOperator
         ):
@@ -74,7 +80,12 @@ class MatmulLinearOperator(LinearOperator):
         else:
             return super()._diagonal()
 
-    def _getitem(self, row_index, col_index, *batch_indices):
+    def _getitem(
+        self,
+        row_index: IndexType,
+        col_index: IndexType,
+        *batch_indices: IndexType,
+    ) -> LinearOperator:
         # Make sure we're not generating more memory with our "efficient" method
         if torch.is_tensor(row_index) and torch.is_tensor(col_index):
             num_indices = row_index.numel()
@@ -87,13 +98,19 @@ class MatmulLinearOperator(LinearOperator):
         res = MatmulLinearOperator(left_tensor, right_tensor)
         return res
 
-    def _matmul(self, right_linear_op):
-        return self.left_linear_op._matmul(self.right_linear_op._matmul(right_linear_op))
+    def _matmul(
+        self: Float[LinearOperator, "*batch M N"],
+        rhs: Union[Float[torch.Tensor, "*batch2 N C"], Float[torch.Tensor, "*batch2 N"]],
+    ) -> Union[Float[torch.Tensor, "... M C"], Float[torch.Tensor, "... M"]]:
+        return self.left_linear_op._matmul(self.right_linear_op._matmul(rhs))
 
-    def _t_matmul(self, right_linear_op):
-        return self.right_linear_op._t_matmul(self.left_linear_op._t_matmul(right_linear_op))
+    def _t_matmul(
+        self: Float[LinearOperator, "*batch M N"],
+        rhs: Union[Float[Tensor, "*batch2 M P"], Float[LinearOperator, "*batch2 M P"]],
+    ) -> Union[Float[LinearOperator, "... N P"], Float[Tensor, "... N P"]]:
+        return self.right_linear_op._t_matmul(self.left_linear_op._t_matmul(rhs))
 
-    def _bilinear_derivative(self, left_vecs, right_vecs):
+    def _bilinear_derivative(self, left_vecs: Tensor, right_vecs: Tensor) -> Tuple[Optional[Tensor], ...]:
         if left_vecs.ndimension() == 1:
             left_vecs = left_vecs.unsqueeze(1)
             right_vecs = right_vecs.unsqueeze(1)
@@ -106,13 +123,13 @@ class MatmulLinearOperator(LinearOperator):
         right_grad = (right_grad,) if not isinstance(right_grad, tuple) else right_grad
         return left_grad + right_grad
 
-    def _permute_batch(self, *dims):
+    def _permute_batch(self, *dims: int) -> LinearOperator:
         return self.__class__(self.left_linear_op._permute_batch(*dims), self.right_linear_op._permute_batch(*dims))
 
-    def _size(self):
+    def _size(self) -> torch.Size:
         return _matmul_broadcast_shape(self.left_linear_op.shape, self.right_linear_op.shape)
 
-    def _transpose_nonbatch(self, *args):
+    def _transpose_nonbatch(self: Float[LinearOperator, "*batch M N"]) -> Float[LinearOperator, "*batch N M"]:
         return self.__class__(self.right_linear_op._transpose_nonbatch(), self.left_linear_op._transpose_nonbatch())
 
     @cached
