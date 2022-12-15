a89c05e1b2755594edde013bc1f3e49a0719c71a
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2022-12-15 15:48:40 +0100

    added some initial interfaces

--- linear_operator/linear_solvers/__init__.py
+++ linear_operator/linear_solvers/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 
-from .cg_solver import CGSolver
+from .cg_solver import CGSolver, CGSolverIterGP
 from .linear_solver import LinearSolver
 
 
-__all__ = ["CGSolver", "LinearSolver"]
+__all__ = ["CGSolver", "CGSolverIterGP", "LinearSolver"]
--- linear_operator/linear_solvers/cg_solver.py
+++ linear_operator/linear_solvers/cg_solver.py
@@ -11,18 +11,32 @@ from ..operators import LinearOperator
 from .linear_solver import LinearSolver, SolverState
 
 
-class CGSolver(LinearSolver):
-    """
-    TODO
+class CGSolverIterGP(LinearSolver):
+    """Conjugate gradient method.
+
+    :param abstol: Absolute residual tolerance.
+    :param reltol: Relative residual tolerance.
+    :max_iter: Maximum number of iterations.
     """
 
-    def __init__(self, tol: float = 1e-4, max_iter: int = None):
-        # TODO: eventually atol and rtol
-        self.tol = tol
-        self.max_iter = max_iter if max_iter is not None else settings.max_cg_iterations.value()
+    def __init__(
+        self,
+        abstol: float = 1e-5,
+        reltol: float = 1e-5,
+        max_iter: int = None,
+    ):
+        self.abstol = abstol
+        self.reltol = reltol
+        self.max_iter = (
+            max_iter if max_iter is not None else settings.max_cg_iterations.value()
+        )
 
     def solve(
-        self, linear_op: LinearOperator, rhs: Tensor, preconditioner: Optional[Callable] = None, num_tridiag: int = 0
+        self,
+        linear_op: LinearOperator,
+        rhs: Tensor,
+        preconditioner: Optional[Callable] = None,
+        num_tridiag: int = 0,
     ) -> SolverState:
         return utils.linear_cg(
             linear_op._matmul,
@@ -30,6 +44,39 @@ class CGSolver(LinearSolver):
             tolerance=self.tol,
             n_tridiag=num_tridiag,
             max_iter=self.max_iter,
-            max_tridiag_iter=settings.max_lanczos_quadrature_iterations.value(),
+            max_tridiag_iter=min(
+                settings.max_lanczos_quadrature_iterations.value(), self.max_iter
+            ),
+            preconditioner=preconditioner,
+        )
+
+
+class CGSolver(LinearSolver):
+    """Conjugate gradient method.
+    TODO
+    """
+
+    def __init__(self, tol: float = 1e-4, max_iter: int = None):
+        self.tol = tol
+        self.max_iter = (
+            max_iter if max_iter is not None else settings.max_cg_iterations.value()
+        )
+
+    def solve(
+        self,
+        linear_op: LinearOperator,
+        rhs: Tensor,
+        preconditioner: Optional[Callable] = None,
+        num_tridiag: int = 0,
+    ) -> SolverState:
+        return utils.linear_cg(
+            linear_op._matmul,
+            rhs,
+            tolerance=self.tol,
+            n_tridiag=num_tridiag,
+            max_iter=self.max_iter,
+            max_tridiag_iter=min(
+                settings.max_lanczos_quadrature_iterations.value(), self.max_iter
+            ),
             preconditioner=preconditioner,
         )
