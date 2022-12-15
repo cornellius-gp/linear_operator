296d024f4548830b25fcb675b26185183dcaff64
Corwin Joy <corwinjoy@gmail.com>
2022-12-14 16:47:04 -0800

    Upgraded signatures and tests for sum_batch_linear_operator.py

--- test/operators/test_sum_batch_linear_operator.py
+++ test/operators/test_sum_batch_linear_operator.py
@@ -3,8 +3,10 @@
 import unittest
 
 import torch
+from jaxtyping import install_import_hook
 
-from linear_operator.operators import DenseLinearOperator, SumBatchLinearOperator
+with install_import_hook("linear_operator", ("typeguard", "typechecked")):
+    from linear_operator.operators import DenseLinearOperator, SumBatchLinearOperator
 from linear_operator.test.linear_operator_test_case import LinearOperatorTestCase
 
 
