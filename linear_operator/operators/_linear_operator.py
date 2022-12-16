7a828074aca709279c24a7825c0785fd64831224
Corwin Joy <corwinjoy@gmail.com>
2022-12-15 16:28:26 -0800

    Upgraded signatures for _linear_operator.py

--- linear_operator/operators/_linear_operator.py
+++ linear_operator/operators/_linear_operator.py
@@ -9,7 +9,7 @@ import warnings
 from abc import abstractmethod
 from copy import deepcopy
 from types import EllipsisType
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+from typing import Any, Callable, Dict, Optional, Tuple, Union
 
 import numpy as np
 import torch
@@ -43,7 +43,7 @@ _HANDLED_SECOND_ARG_FUNCTIONS = {}
 _TYPES_DICT = {torch.float: "float", torch.half: "half", torch.double: "double"}
 
 
-IndexType = Union[EllipsisType, slice, List[int], torch.LongTensor, int]
+IndexType = Union[EllipsisType, slice, list[int], torch.LongTensor, int]
 
 
 def _implements(torch_function: Callable) -> Callable:
