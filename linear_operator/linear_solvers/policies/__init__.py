4873cd4d5d44ce39b9103289b09b9c4db160d83d
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2023-04-05 09:46:02 +0200

    spectral policy added

--- linear_operator/linear_solvers/policies/__init__.py
+++ linear_operator/linear_solvers/policies/__init__.py
@@ -1,11 +1,13 @@
 from .gradient_policy import GradientPolicy
 from .lanczos_policy import LanczosPolicy
 from .linear_solver_policy import LinearSolverPolicy
+from .spectral_policy import SpectralPolicy
 from .unit_vector_policy import UnitVectorPolicy
 
 __all__ = [
     "LinearSolverPolicy",
     "GradientPolicy",
     "LanczosPolicy",
+    "SpectralPolicy",
     "UnitVectorPolicy",
 ]
