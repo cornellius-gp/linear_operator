d53ee4960ecb21761cf6a9a005506705caab4a8f
Max Balandat <balandat@meta.com>
2023-05-03 08:21:22 -0700

    Make typeguard an "test" dep only (I don't think it's needed at runtime, is it?)

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ except ImportError:
     pass
 
 # Other requirements
-install_requires += ["scipy", "jaxtyping>=0.2.9", "typeguard~=2.13.3"]
+install_requires += ["scipy", "jaxtyping>=0.2.9"]
 
 
 # Get version
@@ -77,11 +77,7 @@ setup(
     install_requires=install_requires,
     extras_require={
         "dev": ["ufmt", "twine", "pre-commit"],
-        "test": [
-            "flake8==5.0.4",
-            "flake8-print==5.0.0",
-            "pytest",
-        ],
+        "test": ["flake8==5.0.4", "flake8-print==5.0.0", "pytest", "typeguard~=2.13.3"],
     },
     test_suite="test",
 )
