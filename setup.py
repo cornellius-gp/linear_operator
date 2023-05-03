171432b8dca40a2ea076059f97640aef55c4af7e
Max Balandat <Balandat@users.noreply.github.com>
2023-05-03 11:37:25 -0400

    Make jaxtyping dep install requirement (#60)
    * Make jaxtyping and typeguard required dependencies
    
    See https://github.com/cornellius-gp/linear_operator/pull/42#issuecomment-1533225278
    
    ALso removes the `packaging` dep, this is transitive through `pytest`: https://github.com/pytest-dev/pytest/blob/main/setup.cfg#L48
    
    * Make typeguard an "test" dep only (I don't think it's needed at runtime, is it?)
    
    * Remove typeguard dep (transitive via jaxtyping)
    
    * Add typeguard restriction to <3.0.0 back in

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ except ImportError:
     pass
 
 # Other requirements
-install_requires += ["scipy"]
+install_requires += ["scipy", "jaxtyping>=0.2.9", "typeguard~=2.13.3"]
 
 
 # Get version
@@ -77,14 +77,7 @@ setup(
     install_requires=install_requires,
     extras_require={
         "dev": ["ufmt", "twine", "pre-commit"],
-        "test": [
-            "flake8==5.0.4",
-            "flake8-print==5.0.0",
-            "jaxtyping>=0.2.9",
-            "packaging",
-            "pytest",
-            "typeguard~=2.13.3",
-        ],
+        "test": ["flake8==5.0.4", "flake8-print==5.0.0", "pytest"],
     },
     test_suite="test",
 )
